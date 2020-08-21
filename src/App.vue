f8d820fc94b87bebf09eaf09b7dc2b1ed62ee921
mouaadh dilmi <dmoadth@gmail.com>
2020-08-21 10:58:26 +0200

    adding the vue project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    ``
+    <Header />
+    <question-block />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import QuestionBlock from "./components/QuestionBlock.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    QuestionBlock,
+  },
+};
 </script>
 
 <style>
